31907d5998eef2e496692b186baacd62d4155b0b
Dorothea Leite <dorothea.leite@mds.torrens.edu.au>
2021-04-02 14:02:56 +0800

    Question 2 added here

---
+++ ADS103_Question1_Assessment2_DorotheaLeite_1/AssignmentPartB/Node.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+class Node
+{
+public:
+	int number;
+
+	Node* next;
+
+};
+
---
+++ ADS103_Question1_Assessment2_DorotheaLeite_1/AssignmentPartB/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <stdlib.h>
+#include <fstream>
+#include <windows.h>
+#include <vector>
+
+using namespace std;
+
+void main() {
+
+
+
+
+
+
+	system("pause");
+}
